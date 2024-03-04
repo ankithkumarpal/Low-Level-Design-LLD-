d91315ea955ed812bf5e9e5cde96da4768d871e7
ankithkumarpal <ankithpal721@gmail.com>
2024-03-04 23:59:23 +0530

    add single responsibility code

---
+++ SOLID-PRINCIPLES/SingleResponsibility/SngleResp.java
@@ -0,0 +1,30 @@
+class Employee {
+    String  Name; 
+    String Address ; 
+}
+
+class saveEmployee {
+    Employee emp ; 
+    saveEmployee(Employee emp) {
+        this.emp = emp;
+    }
+
+    public void saveEmp(Employee emp) {
+        // saves employee to db
+    }
+}
+
+class generatePayroll {
+    Employee emp ; 
+    generatePayroll(Employee emp) {
+        this.emp = emp;
+    }
+
+    public void gerneratePay(Employee emp) {
+        // generate pay roll
+    }
+}
+
+public class SngleResp {
+    
+}
---
+++ SOLID-PRINCIPLES/SingleResponsibility/ruleVoilationCode.java
@@ -0,0 +1,20 @@
+
+class Employee {
+    String Name ; 
+    String Address ; 
+
+    public void saveEmployee(Employee emp) {
+        // saves employee to db
+    }
+
+    public void generatePayroll(Employee emp) {
+        // generates employee payroll
+    }
+}
+
+
+class ruleViolationCode{
+  public static void  main(String [] args) {
+
+  }
+}
